1c829dc1853328994ff419b4d5d407b912421589
timnoot <t.t.nootenboom@gmail.com>
2022-07-27 11:20:31 +0200

    Updates

---
+++ precache-manifest.12e48947f890d88a915ff961a5ad8b1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f75e76c203c79f86adb",
+    "url": "/static/css/main.1764749c.chunk.css"
+  },
+  {
+    "revision": "3f75e76c203c79f86adb",
+    "url": "/static/js/main.3f75e76c.chunk.js"
+  },
+  {
+    "revision": "5e84b6c8991ba5df7c1d",
+    "url": "/static/css/1.f6f766f7.chunk.css"
+  },
+  {
+    "revision": "5e84b6c8991ba5df7c1d",
+    "url": "/static/js/1.5e84b6c8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "447c6174edf22eeb56d84d312763743f",
+    "url": "/static/media/redexclamation.447c6174.svg"
+  },
+  {
+    "revision": "48253677d57bed3ae05b3b68c5be162f",
+    "url": "/index.html"
+  }
+];
